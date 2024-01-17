285849a5e10324bf4469b1142721c3fb5b340018
Marek Trybuła <trybu001@gmail.com>
2024-01-17 09:49:12 +0100

    Solve problem with bootstrap styles Make MainNavigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import MainNavigation from './components/navigation/MainNavigation'
+import './styles/index.scss'
+
 function App() {
-  return <h1>Company website</h1>
+  return (
+    <div>
+      <MainNavigation />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/navigation/MainNavigation.jsx
@@ -0,0 +1,55 @@
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+
+export default function MainNavigation() {
+  return (
+    <Navbar key={'lg'} expand={'lg'} className="bg-body-tertiary mb-3">
+      <Container fluid>
+        <Navbar.Brand href="#">Codemates</Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'lg'}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${'lg'}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${'lg'}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'lg'}`}>
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Search for codemate</Nav.Link>
+              <Nav.Link href="#action2">Post an add</Nav.Link>
+              <NavDropdown
+                title="Marek Trybula"
+                id={`offcanvasNavbarDropdown-expand-${'lg'}`}
+              >
+                <NavDropdown.Item href="#action3">Account</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">Log out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  )
+}
